a6a801060a001a806a12a2a5266840342b2f74c3
palstr <palstrgames@gmail.com>
2024-09-06 10:11:46 +0200

    Losowanie liczby rzeczywistej

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Na lekcjach");
+
+        double wylosowanaLiczba = Math.random()*100+1;
+
+        System.out.println(wylosowanaLiczba);
     }
 
 
