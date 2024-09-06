68f6eb6c32ca5e23169daa24a37ae5f36878f302
palstr <palstrgames@gmail.com>
2024-09-06 10:26:10 +0200

    Wczytywanie liczby z klawiatury

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,32 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("Na lekcjach");
 
         int wylosowanaLiczba = (int) (Math.random()*100+1);
-        //rzutowanie na inta ^ (wyrażane nawiasem)
+        //rzutowanie na inta     ^ (wyrażane nawiasem)
+        /*
+        typy proste przechowują tylko wartość (pisane małą literą!!):
+        logiczne - boolean
+        całkowite - byte, short, int, long
+        rzeczywiste - double, float
+        znakowy - char
+
+        typy złożone (mają własności i metody):
+        String, Integer, Kosc
+         */
 
         System.out.println(wylosowanaLiczba);
+
+        //wczytywanie z klawiatury
+
+        Scanner klawiatura = new Scanner(System.in);
+        System.out.println("Podaj liczbę");
+        int wpisanaLiczba = klawiatura.nextInt();
+
+
     }
 
 
